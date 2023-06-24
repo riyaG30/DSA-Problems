e31b928aeb137a653c30c8c509f7c602e4c9390e
riyaG30 <123980537+riyaG30@users.noreply.github.com>
2023-06-24 09:10:14 +0530

    Added solution - LeetHub

--- Number of Unique Paths - GFG/number-of-unique-paths.cpp
+++ Number of Unique Paths - GFG/number-of-unique-paths.cpp
@@ -11,23 +11,45 @@ class Solution
     public:
     //Function to find total number of unique paths.
     
-    int solve(int i,int j, vector<vector<int>> &dp )
+    int solve1(int i,int j, vector<vector<int>> &dp ) // memorozation
     {
         if(i == 0 && j ==0)
         return 1;
         if(i <0 || j <0) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        int up = solve(i-1,j,dp);
-        int left = solve(i,j-1,dp);
+        int up = solve1(i-1,j,dp);
+        int left = solve1(i,j-1,dp);
         return dp[i][j] = up+left;
     }
     
+    
+    int solve(int a, int b,vector<vector<int>> &dp)
+    {
+        for(int i = 0; i <a;i++)
+        {
+            for(int j  = 0; j <b; j++)
+            {
+                if(i == 0 && j == 0)
+                dp[0][0] = 1;
+                else
+                {
+                    int up = 0,left = 0;
+                    if(i >0)  up = dp[i-1][j];
+                    if(j >0)  left = dp[i][j-1];
+                    dp[i][j] = up+left;
+                }
+                
+            }
+        }
+        return dp[a-1][b-1];
+    }
     
     int NumberOfPath(int a, int b)
     {
         //code here
         vector<vector<int>> dp(a,(vector<int>(b,-1)));
-      return   solve(a-1,b-1,dp);
+     // return   solve1(a-1,b-1,dp);   //  memorization
+       return   solve(a,b,dp);   //  Tabulation
         
     }
 };
