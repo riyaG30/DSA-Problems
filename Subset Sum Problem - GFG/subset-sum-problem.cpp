fa94c61bb76767ab5bd439952265f19a7665ef67
riyaG30 <123980537+riyaG30@users.noreply.github.com>
2023-06-26 20:32:35 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -9,26 +9,54 @@ using namespace std;
 
 class Solution{   
 public:
-        bool solve(int ind,int target,vector<int> arr,vector<vector<int>> &dp)
+
+     // memorization
+        bool solve1(int ind,int target,vector<int> arr,vector<vector<int>> &dp)
         {
             if(target == 0) return true;
             if(ind == 0) return (arr[0] == target);
             if(dp[ind][target] != -1) return dp[ind][target];
-            bool not_take = solve(ind-1,target,arr,dp);
+            bool not_take = solve1(ind-1,target,arr,dp);
             bool take = false;
             if(target >= arr[ind])
-            take = solve(ind-1,target-arr[ind],arr,dp);
+            take = solve1(ind-1,target-arr[ind],arr,dp);
             return dp[ind][target] = (take || not_take);
         }
-
-
+        bool solve(vector<int> arr, int target)
+{
+    int n = arr.size();
+    vector<vector<bool>> dp(n, vector<bool>(target + 1));
+    
+    for (int i = 0; i < n; i++)
+    {
+        dp[i][0] = true; // for all the targets = 0, it will be true
+    }
+    
+    dp[0][arr[0]] = true;
+    
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j <= target; j++)
+        {
+            bool not_take = dp[i - 1][j];
+            bool take = false;
+            if (j >= arr[i])
+                take = dp[i - 1][j - arr[i]];
+            dp[i][j] = (take || not_take);
+        }
+    }
+    
+    return dp[n - 1][target];
+}
 
     bool isSubsetSum(vector<int>arr, int target){
         // code here 
         int n = arr.size();
         
         vector<vector<int>> dp(n,vector<int>((target+1),-1));
-       return solve(n-1,target,arr,dp);
+      // return solve1(n-1,target,arr,dp);   // memorization
+
+      return solve(arr,target);
     }
 };
 
