1c47b9841ba647ab3d2ff064129f38f0085d7658
riyaG30 <123980537+riyaG30@users.noreply.github.com>
2023-07-06 12:48:31 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -5,47 +5,27 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  bool detect(int src,vector<int>adj[],int vis[])
-  {
-      vis[src] =1; 
-      queue<pair<int,int>> q;
-        q.push({src,-1});
-        while(!q.empty())
-        {
-            int node = q.front().first;
-            int parent = q.front().second;
-            q.pop();
-            for(auto it :adj[node])
+    // Function to detect cycle in an undirected graph.
+    bool dfs(int node,int parent, int vis[],vector<int> adj[])
+    {
+        vis[node] = 1;
+        for(auto adjacentNode : adj[node]){
+            if(!vis[adjacentNode])
             {
-                if(!vis[it])
-                {
-                    vis[it] =1;
-                    q.push({it,node});
-                    
-                }
-                // if it has been visited and it was not in the path
-                else if(parent != it)
-                {
-                   return true; 
-                }
+                if(dfs(adjacentNode,node,vis,adj)) return true;
+            
             }
+            else if(adjacentNode != parent) return true;
         }
         return false;
-        
-  }
-    // Function to detect cycle in an undirected graph.
-    
-    bool isCycle(int N, vector<int> adj[]) {
-        int vis[N] = {0};
-        // now if there are multiple connected components
-        for(int i = 0; i <N; i++)
+    }
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+        int vis[V] = {0};
+        for(int i = 0; i <V;i++)
         {
             if(!vis[i])
-            {
-                if(detect(i,adj,vis)) return true;
-            }
-        
-            
+           if(dfs(i,-1,vis,adj)) return true; 
         }
         return false;
     }
