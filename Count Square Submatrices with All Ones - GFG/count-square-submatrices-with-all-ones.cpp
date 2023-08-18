bf12902f18bb9a32491b53753d69649500bb9df8
riyaG30 <123980537+riyaG30@users.noreply.github.com>
2023-08-18 19:10:40 +0530

    Added solution - LeetHub

---
+++ Count Square Submatrices with All Ones - GFG/count-square-submatrices-with-all-ones.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int countSquares(int m, int n, vector<vector<int>> &arr) {
+         vector<vector<int>> dp(m,vector<int>(n,0));
+        for(int i = 0 ; i <m; i++) dp[i][0] = arr[i][0];
+        for(int j =0; j < n; j++) dp[0][j] = arr[0][j];
+
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 1; j <n; j++)
+            {
+                if(arr[i][j] == 0) dp[i][j] = 0;
+                else
+                {
+                    dp[i][j] = 1+ min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+                }
+            }
+        }
+    int sum = 0;
+     for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j <n; j++)
+            {
+                sum += dp[i][j];
+            }
+        }   
+return sum;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M;
+        cin >> N >> M;
+
+        vector<vector<int>> matrix(N, vector<int>(M));
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+        Solution obj;
+        cout << obj.countSquares(N, M, matrix) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
